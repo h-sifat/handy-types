a39a7f51e5e1c133212a7ca2e0af689775d58426
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-08-14 11:29:48 +0600

    feat(schema-parser): generate schema name
    From now on the schema parser also generates schema name. For example
    `parseSchema("string[]")` returns the object
    
    ```js
    {
      schemaType: "array",
      schemaName: "Array of String",
      elementType: "string"
    }
    ```
    The `schemaName` will be used to generate usefull error messages for the
    `assert` function that will be added later inshaAllah.

--- src/schema-parser.ts
+++ src/schema-parser.ts
@@ -1,14 +1,16 @@
-import { handyTypes, AllHandyTypes } from "./types";
+import { handyTypes, AllHandyTypes, typeNames } from "./types";
 import { EPP, assertValidHandyType } from "./util";
 
 interface BasicSchemaDefinition {
   schemaType: "basic";
   type: AllHandyTypes;
+  schemaName: string;
 }
 
 interface ArraySchemaDefinition {
   schemaType: "array";
   elementType: AllHandyTypes;
+  schemaName: string;
 }
 
 type UnionSubSchema = BasicSchemaDefinition | ArraySchemaDefinition;
@@ -16,6 +18,7 @@ type UnionSubSchema = BasicSchemaDefinition | ArraySchemaDefinition;
 interface UnionSchemaDefinition {
   schemaType: "union";
   subSchemas: UnionSubSchema[];
+  schemaName: string;
 }
 
 export type SchemaDefinition = UnionSubSchema | UnionSchemaDefinition;
@@ -25,20 +28,32 @@ export default function parseSchema(schema: string): SchemaDefinition {
     const subSchemas = schema
       .split("|")
       .map((subSchema) => subSchema.trim())
-      .map(parseSchema);
+      .map(parseSchema) as UnionSubSchema[];
 
-    return { schemaType: "union", subSchemas: subSchemas as UnionSubSchema[] };
+    const schemaName = subSchemas
+      .map((subSchema) => subSchema.schemaName)
+      .join(" or ");
+
+    return { schemaType: "union", subSchemas, schemaName };
   }
 
   if (schema.endsWith("[]")) {
-    const elementType = schema.slice(0, -2); // remove "[]"
+    const elementType = schema.slice(0, -2) as AllHandyTypes; // remove "[]"
     assertValidHandyType(elementType);
 
-    return { schemaType: "array", elementType: elementType as AllHandyTypes };
+    return {
+      elementType,
+      schemaType: "array",
+      schemaName: `Array of ${typeNames[elementType]}`,
+    };
   }
 
   if (schema in handyTypes)
-    return { schemaType: "basic", type: schema as AllHandyTypes };
+    return {
+      schemaType: "basic",
+      type: schema as AllHandyTypes,
+      schemaName: typeNames[schema as AllHandyTypes],
+    };
 
   throw new EPP(`Invalid handy type: "${schema}".`, "INVALID_HANDY_TYPE");
 }
--- tests/schema-parser.test.ts
+++ tests/schema-parser.test.ts
@@ -1,22 +1,40 @@
 import parseSchema from "../src/schema-parser";
+import { typeNames } from "../src/types";
 
 describe("input/output", () => {
   it.each([
     {
       schema: "number",
-      parsedSchema: { schemaType: "basic", type: "number" },
+      parsedSchema: {
+        schemaType: "basic",
+        type: "number",
+        schemaName: typeNames["number"],
+      },
     },
     {
       schema: "string[]",
-      parsedSchema: { schemaType: "array", elementType: "string" },
+      parsedSchema: {
+        schemaType: "array",
+        elementType: "string",
+        schemaName: "Array of String",
+      },
     },
     {
       schema: "string[] | number",
       parsedSchema: {
         schemaType: "union",
+        schemaName: `Array of String or ${typeNames["number"]}`,
         subSchemas: [
-          { schemaType: "array", elementType: "string" },
-          { schemaType: "basic", type: "number" },
+          {
+            schemaType: "array",
+            elementType: "string",
+            schemaName: `Array of ${typeNames["string"]}`,
+          },
+          {
+            type: "number",
+            schemaType: "basic",
+            schemaName: typeNames["number"],
+          },
         ],
       },
     },
